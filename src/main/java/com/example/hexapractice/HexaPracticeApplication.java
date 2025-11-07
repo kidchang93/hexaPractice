93376d3dc63da3d512648705b03f02d9220f3393
cklee93 <lck930721@guidance.co.kr>
2025-11-07 09:23:37 +0900

    Initial commit

---
+++ src/main/java/com/example/hexapractice/HexaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexaPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hexapractice/HexaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
