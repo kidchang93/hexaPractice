5a99f3735e73a4ca51a9ee93be616b050bccd433
cklee93 <lck930721@guidance.co.kr>
2025-11-07 14:35:52 +0900

    주석, yml 정보 변경

--- src/main/java/com/example/hexapractice/common/utils/UseCase.java
+++ src/main/java/com/example/hexapractice/common/utils/UseCase.java
@@ -3,7 +3,20 @@ package com.example.hexapractice.common.utils;
 import jakarta.validation.Valid;
 import org.springframework.validation.annotation.Validated;
 
+/**
+ * 공통 UseCase 인터페이스
+ * 
+ * @param <T> 입력 타입 (Command/Query)
+ * @param <R> 반환 타입 (Domain/DTO)
+ */
 @Validated
 public interface UseCase<T, R> {
-    R run(@Valid T Request);
+    
+    /**
+     * UseCase 실행
+     * 
+     * @param request 요청 데이터 (자동 검증됨)
+     * @return 실행 결과
+     */
+    R run(@Valid T request);
 }
