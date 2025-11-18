61dfdc2d944558cfa9a8e63eeaeda2ba102251ce
cklee93 <lck930721@guidance.co.kr>
2025-11-18 09:48:36 +0900

    feat: save 기능 추가

--- src/main/java/com/example/hexapractice/adapter/out/persistence/KTLProductPersistenceAdapter.kt
+++ src/main/java/com/example/hexapractice/adapter/out/persistence/KTLProductPersistenceAdapter.kt
@@ -4,11 +4,13 @@ import com.example.hexapractice.application.port.out.KTLDeleteProductPort
 import com.example.hexapractice.application.port.out.KTLLoadProductPort
 import com.example.hexapractice.application.port.out.KTLSaveProductPort
 import com.example.hexapractice.domain.KTLProduct
+import org.springframework.stereotype.Component
 import java.util.Optional
 
 /**
  * 구현체 작성
  */
+@Component
 class KTLProductPersistenceAdapter(
     private val ktlProductJpaRepository: KTLProductJpaRepository,
     private val ktlProductMapper: KTLProductMapper,
@@ -27,9 +29,22 @@ class KTLProductPersistenceAdapter(
         TODO("Not yet implemented")
     }
 
-    override fun save(product: KTLProduct): KTLProduct {
-        TODO("Not yet implemented")
-    }
+    /**
+     * 자바 스타일
+     */
+//    override fun save(product: KTLProduct): KTLProduct {
+//        val entity = ktlProductMapper.toEntity(product)
+//        val savedEntity = ktlProductJpaRepository.save(entity);
+//        return ktlProductMapper.toDomain(savedEntity);
+//    }
+    /**
+     * 코틀린 스럽게 바꾸기
+     */
+    override fun save(product: KTLProduct): KTLProduct =
+        ktlProductMapper.toEntity(product)
+            .let { ktlProductJpaRepository.save(it) }
+            .let { ktlProductMapper.toDomain(it) }
+
 
     override fun deleteProduct(id: Long) {
         TODO("Not yet implemented")
--- src/main/java/com/example/hexapractice/application/port/out/KTLSaveProductPort.kt
+++ src/main/java/com/example/hexapractice/application/port/out/KTLSaveProductPort.kt
@@ -3,5 +3,6 @@ package com.example.hexapractice.application.port.out
 import com.example.hexapractice.domain.KTLProduct
 
 interface KTLSaveProductPort{
+
     fun save(product: KTLProduct): KTLProduct
 }
